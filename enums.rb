ba2c9bf6d4cae725ee0c976efc423042fca2093c
Fernando Rivas <ferm0494@Fernandos-MacBook-Pro.local>
2020-05-09 16:07:16 -0600

    Returning Enum and used for not each TSE observations

--- enums.rb
+++ enums.rb
@@ -1,85 +1,68 @@
-# rubocop:disable Metrics/ModuleLength
+# rubocop:disable Style/For
 module Enumerables
   def my_each
-    if block_given?
-      each do |element|
-        yield(element)
-      end
-    else
-      puts 'Block wasnt given'
-    end
+    return unless block_given?
+
+    for element in self
+      yield(element)
+  end
   end
 
   def my_each_with_index
-    if block_given?
-      (0...size).each do |i|
-        yield(i, self[i])
-      end
-    else
-      puts 'Block wasnt given'
-    end
+    return unless block_given?
+
+    for i in (0...size)
+      yield(i, self[i])
+end
   end
 
   def my_select
     if block_given?
-
       arr = []
       my_each do |element|
         arr << element if yield(element)
       end
-    else
-      puts 'Block wasnt given'
     end
-
     arr
   end
 
   def my_all?
-    response = true
     if block_given?
+      response = true
       my_each do |element|
         response = false unless yield(element)
       end
-
-    else
-      puts 'Block wasnt given'
     end
     response
   end
 
   def my_any?
-    response = false
     if block_given?
+      response = false
       my_each do |element|
         response = true if yield(element)
       end
-
-    else
-      puts 'Block wasnt given'
     end
     response
   end
 
   def my_none?
-    response = false
     if block_given?
+      response = false
       my_each do |element|
         response = true if yield(element)
       end
-    else
-      puts 'Block wasnt given'
     end
     response
   end
 
   def my_count
-    count = 0
     if block_given?
+      count = 0
       my_each do |element|
         count += 1 if yield(element)
       end
     else
-      puts 'Block wasnt given'
       count = size
     end
     count
@@ -87,7 +70,6 @@ module Enumerables
 
   def my_map(proc = nil)
     arr = []
-
     if proc.equal?(nil) == false
       my_each do |element|
         arr << proc.call(element)
@@ -96,9 +78,6 @@ module Enumerables
       my_each do |element|
         arr << yield(element)
       end
-    else
-      return 'Block wasnt given'
-
     end
     arr
   end
@@ -120,8 +99,6 @@ module Enumerables
       my_each do |element|
         acum = yield(acum, element)
       end
-    else
-      puts 'Block wasnt given'
     end
     acum
   end
@@ -130,4 +107,4 @@ module Enumerables
     my_inject(:*)
   end
 end
-# rubocop:enable Metrics/ModuleLength
+# rubocop:enable Style/For
