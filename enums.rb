5eda9a449085ce54345f7b7256746bcdb7f77ce2
Fernando Rivas <ferm0494@Fernandos-MacBook-Pro.local>
2020-05-12 13:03:00 -0600

    removed comments

--- enums.rb
+++ enums.rb
@@ -236,20 +236,3 @@ end
 # rubocop:enable Metrics/PerceivedComplexity
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/MethodLength
-
-# ar = "adfjos".chars.sort
-# ar2 ="aefiou".chars.sort
-# print ar
-# print ar2
-# count = 0 
-
-# for i in (0 ... ar2.size)
-#   if  ar2[i].eql?( ar[i])
-#       ar[i] == ar[2]
-#   else
-#     count +=1
-
-#   end
-# end
-
-# puts "FINAL #{count}"
